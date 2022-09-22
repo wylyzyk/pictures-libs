e655d5e6e95ba9058ed6c13ff9fb7e4880569f1a
wangyl <wylyzyk1028@gmail.com>
2022-09-23 00:38:33 +0800

    feat(directives): auto register directives

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,3 +1,7 @@
+/**
+ * 指令的自动注册(vite)
+ */
+
 import lazy from "./modules/lazy";
 
 /**
@@ -5,6 +9,14 @@ import lazy from "./modules/lazy";
  */
 export default {
   install(app) {
-    app.directive("lazy", lazy);
+    // 使用globEager同步导入
+    const directives = import.meta.globEager("./modules/*.js");
+    for (const [key, value] of Object.entries(directives)) {
+      // 获取指令名称
+      const arr = key.split("/");
+      const name = arr[arr.length - 1].replace(".js", "");
+      // 注册指令
+      app.directive(name, value.default);
+    }
   }
 };
